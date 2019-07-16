1b4cd17f98b4470f367509846ffdac7ae13dc36b
Adam Surgenor <adam@surgenor.me>
2019-07-16 08:56:20 +0100

    async test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,16 +1,15 @@
 import server from '../server/index';
 import request from 'supertest';
+import 'babel-polyfill';
 
 afterEach(() => 
   server.close());
 
 describe('get exchange rate', () => {
-  test('correct exchange rate for USD to EUR', () => {
-    return request(server).get('/exchange-rate/USD/to/EUR')
-    .then(response => {
-        expect(response.status).toEqual(200);
-        expect(response.type).toEqual('application/json');
-        expect(response.body.rate).toEqual(0.87815);
-            })
+  test('correct exchange rate for USD to EUR', async () => {
+    const response = await request(server).get('/exchange-rate/USD/to/EUR');
+      expect(response.status).toEqual(200);
+      expect(response.type).toEqual('application/json');
+      expect(response.body.rate).toEqual(0.87815);
   });
 });
