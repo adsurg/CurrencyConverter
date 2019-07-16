9ffb4532ab7b1d76b4a101bec929a51a4ab933d3
Adam Surgenor <adam@surgenor.me>
2019-07-16 16:11:58 +0100

    Convert implemented

--- server/index.js
+++ server/index.js
@@ -6,9 +6,20 @@ const router = new Router();
 const app = new Koa();
 const PORT = process.env.PORT || 8081;
 
+const round = value => Math.round(value * 100) / 100; 
+
 router.get('/exchange-rate/:source/to/:destination', ctx => {
   ctx.body = {
     rate: exchangeRate(ctx.params.source, ctx.params.destination)
+  };
+});
+
+router.get('/convert/:value/:source/to/:destination', ctx => {
+  const rate = exchangeRate(ctx.params.source, ctx.params.destination);
+  ctx.body = {
+    value: round(Number(ctx.params.value) * rate),
+    rate,
+    currency: ctx.params.destination
   };
 });
 
