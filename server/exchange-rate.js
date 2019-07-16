a410b8e706d3c54fdd68fdd0585a40fbff9d51b6
Adam Surgenor <adam@surgenor.me>
2019-07-16 15:51:46 +0100

    Bi-directional exchange rate

--- server/exchange-rate.js
+++ server/exchange-rate.js
@@ -1,3 +1,4 @@
+const baseCurrency = 'USD';
 const rateLookup = {
     'EUR': 0.87815,
     'GBP': 0.78569,
@@ -8,8 +9,17 @@ const rateLookup = {
     'CNY': 6.88191,
     'MYR': 4.13785,
     'COP': 3203.18,
-}
+};
 
-const exchangeRate = (source, destination) => rateLookup[destination];
+const exchangeRate = (source, destination) => {
+    let rate = 1;
+    if(source != baseCurrency)
+        rate /= rateLookup[source];
+
+    if(destination != baseCurrency)
+        rate *= rateLookup[destination];
+
+    return rate;
+}
 
 export default exchangeRate;
--- test/index.spec.js
+++ test/index.spec.js
@@ -6,10 +6,17 @@ afterEach(() =>
   server.close());
 
 describe('get exchange rate', () => {
-  test('correct exchange rate for USD to EUR', async () => {
-    const response = await request(server).get('/exchange-rate/USD/to/EUR');
-      expect(response.status).toEqual(200);
-      expect(response.type).toEqual('application/json');
-      expect(response.body.rate).toEqual(0.87815);
-  });
+    test('correct exchange rate for USD to EUR', async () => {
+        const response = await request(server).get('/exchange-rate/USD/to/EUR');
+          expect(response.status).toEqual(200);
+          expect(response.type).toEqual('application/json');
+          expect(response.body.rate).toBeCloseTo(0.87815, 5);
+      });
+
+      test('correct exchange rate for GBP to USD', async () => {
+        const response = await request(server).get('/exchange-rate/GBP/to/USD');
+          expect(response.status).toEqual(200);
+          expect(response.type).toEqual('application/json');
+          expect(response.body.rate).toBeCloseTo(1.27277, 5);
+      });
 });
